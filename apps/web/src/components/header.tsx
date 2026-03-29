d745fd78b34a6f655857ffb929f977177c204373
jagrat7 <jagratrao@gmail.com>
2026-03-28 23:43:39 -0400

    remove sign-in button from header, keep only user button for signed-in users

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -43,9 +43,6 @@ export default function Header() {
             ))}
           </nav>
           <ModeToggle />
-          <Show when="signed-out">
-            <SignInButton />
-          </Show>
           <Show when="signed-in">
             <UserButton />
           </Show>
